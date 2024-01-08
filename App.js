676633e761ca0d7603d604dd7c99498919068595
filipesag <filipesag@hotmail.com>
2024-01-08 15:49:13 -0300

    Fonte customizável criada com dir. de componente

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { StatusBar, SafeAreaView } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
-import Cesta from './src/screens/Cesta';
+import Cesta from './src/screen/Cesta';
 
 export default function App() {
+  const [fontCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  if(!fontCarregada){
+    return <View />
+  }
+
+
   return (
     <SafeAreaView>
       <StatusBar />
