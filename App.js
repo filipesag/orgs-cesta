11beb0ad7c6d1a566634fffef79a71ef80af4ec7
filipesag <filipesag@hotmail.com>
2024-01-09 11:47:46 -0300

    Adicionado app loading

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StatusBar, SafeAreaView, View } from 'react-native';
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import mock from './src/mocks/cesta'
 import Cesta from './src/screen/Cesta/Cesta';
+import AppLoading  from 'expo-app-loading';
 
 export default function App() {
   const [fontCarregada] = useFonts({
@@ -11,7 +12,7 @@ export default function App() {
   });
 
   if(!fontCarregada){
-    return <View />
+    return <AppLoading />
   }
 
   return (
